8f4b2a6d2a157ff288926de449a946f125fa55e0
Jay Lowry <jay.lowry350@gmail.com>
2023-09-14 17:45:04 -0600

    Coodling

--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -25,7 +25,35 @@ public class Trie implements ITrie {
 
     @Override
     public INode find(String word) {
+        word = word.toLowerCase();
+        int index = word.charAt(0) - 'a';
+        var nextNode = root.children[index];
+        while (nextNode != null) {
+            word = word.substring(1);
+            nextNode = traverseTrie(nextNode, word);
+        }
+
+
+
+        /*
+        1. Start at root  ***
+        2. Check if root child exists for first letter ***
+        3. Go to root child ***
+        4. Pop off first letter of word ***
+        5. Check if next node child exists for new first letter
+        6. Go to next node child
+        7. Repeat steps 4-6 until word is finished
+         */
         return null;
+    }
+
+    private INode traverseTrie(INode node, String word) {
+        if (word == null) {
+            return null;
+        }
+        int index = word.charAt(0) - 'a';
+        var nextNode = node.children[index];
+        return nextnode;
     }
 
     @Override
